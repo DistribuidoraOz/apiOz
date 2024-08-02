d84a03fb3969b0043f84ed6393d11a6022218fc5
Sebastian Rossi <ssebass.ross@gmail.com>
2024-08-01 23:41:06 -0300

    se modifico router.js, endpoints, nombres y respuestas

--- src/router/router.js
+++ src/router/router.js
@@ -1,14 +1,19 @@
-import express, { json } from 'express';
+import express from 'express';
 import { Categorias, Marcas, Productos } from '../models/models.js';
 import { upload } from '../lib/cloudinary.js'
-import { where } from 'sequelize';
 
 export const router = express.Router();
 
+/////////// ruta inicial  /////////////////
+
+router.get('/', (req, res)=>{
+    res.status(200).json({message: "Welcome to api DistribuidoraOz!"});
+});
+
+
 ///////////  categorias rutas /////////////////
 
-router.get('/categoria/:id', async(req, res)=>{
-    console.log("categoria por id");
+router.get('/categoryById/:id', async(req, res)=>{
 
     try {
 
@@ -16,55 +21,60 @@ router.get('/categoria/:id', async(req, res)=>{
             attributes: ['nombre'],
             where: {id: req.params.id}
         });
-        //console.log("categoria recuperada: ", category)
-        res.status(200).json(category);
+        if(category === null){
+            res.status(404).json({message: "No se encontro categoria con ese id!"});
+        }else{
+            res.status(200).json(category);
+        }      
     } catch (error) {
-        console.log("Error categoria by id: ", error);
-        res.status(500).json({message: 'Error al recuperar categoria por id!'})
+        console.log("Error al recuperar categoria por id: ", error);
+        res.status(500).json({message: 'Error al recuperar categoria por id!'});
     }
 });
 
-router.get('/categorias', async(req, res)=>{
+router.get('/categorys', async(req, res)=>{
     //console.log("FFFFFFFFFFFFFFFFFFFFcategory");
     try {
         const categorys = await Categorias.findAll({
             attributes: ['id', 'nombre'],
             order: ['nombre'],
         });
-
-        const categoryList = categorys.map((item)=>({
-            id: item.id,
-            nombre : item.nombre
-        }));
-
-        //console.log('se recuperaron con exito las Marcas ', marcasList);
-        res.status(200).json(categoryList);
+        if(categorys.length === 0){
+            res.status(404).json({message: "No se recupero lista de categorias!"});
+        }else{
+            const categoryList = categorys.map((item)=>({
+                id: item.id,
+                nombre : item.nombre
+            }));
+            res.status(200).json(categoryList);
+        }        
     } catch (error) {
-        console.log("error al recuperar los datosXXXX: ", error);
-        res.status(500).json({error: 'Error al recuperar all category!'});
+        console.log("Error al recuperar lista de categorias: ", error);
+        res.status(500).json({error: 'Error al recuperar lista de categorias!'});
     }
 });
 
 ///////////// MARCAS RUTAS ////////////////
 
-router.get('/marcas/:id', async(req, res)=>{
+router.get('/marcasByCategoryId/:id', async(req, res)=>{
     try {
         const marcas = await Marcas.findAll({
             attributes: ['id', 'nombre'],
             where: { CategoriaId: req.params.id},
             order: ['nombre'],
         });
-
-        const marcasList = marcas.map((item)=>({
-            id: item.id,
-            nombre : item.nombre
-        }));
-
-        //console.log('se recuperaron con exito las Marcas ', marcasList);
-        res.status(200).json(marcasList);
+        if(marcas.length === 0){
+            res.status(404).json({message: "No se encontraron marcas con esa categoria id!"});
+        }else{
+            const marcasList = marcas.map((item)=>({
+                id: item.id,
+                nombre : item.nombre
+            }));
+            res.status(200).json(marcasList);
+        }        
     } catch (error) {
-        console.log("error al recuperar los datos: ", error);
-        res.status(500).json({error: 'Error al recuperar Marcas!'});
+        console.log("Error al recuperar marcas por categoria id: ", error);
+        res.status(500).json({error: 'Error al recuperar marcas por categoria id!'});
     }
 });
 
@@ -73,76 +83,65 @@ router.get('/marcas', async(req, res)=>{
         const marcas = await Marcas.findAll({
             attributes: ['id', 'nombre', 'CategoriaId'],
         });
-
-        const marcasList = marcas.map((item)=>({
-            id: item.id,
-            nombre : item.nombre,
-            categoriaId: item.CategoriaId
-        }));
-
-        console.log('se recuperaron con exito TODAS las Marcas ');
-        res.status(200).json(marcasList);
+        if(marcas.length === 0){
+            res.status(404).json({message: "No se recupero lista de marcas!"});
+        }else{
+            const marcasList = marcas.map((item)=>({
+                id: item.id,
+                nombre : item.nombre,
+                categoriaId: item.CategoriaId
+            }));
+            res.status(200).json(marcasList);
+        }
     } catch (error) {
-        console.log("error al recuperar los datos: ", error);
-        res.status(500).json({error: 'Error al recuperar Marcas!'});
+        console.log("error al recuperar lista de marcas: ", error);
+        res.status(500).json({error: 'Error al recuperar lista de Marcas!'});
     }
 });
 
-router.get('/marca/:id', async(req, res)=>{
-    try {
-        console.log("addddddddadadadadadddddddd")
-        const marca = await findOne({
-            attributes: ['nombre'],
-            where: {id: req.params.id}
-        });
-        res.status(200).json(marca);
-    } catch (error) {
-        console.log("agagagagagaTTTTTTTTTTTTTTTTTTT")
-        res.status(500),json({mesaje: 'No se pudo recuperar marca por id.'})
-    }
-});
 ////////////   PRODUCTOS RUTAS    /////////////////
 
-router.get('/productos/:id', async(req, res)=>{
+router.get('/productosByMarcaId/:id', async(req, res)=>{
     try {
-        const response = await Productos.findAll({
+        const productos = await Productos.findAll({
             attributes: ['id', 'nombre', 'descripcion', 'imagen'],
             where: { MarcaId: req.params.id },
             order: ['nombre'],
         });
-
-        const data = response.map((item)=>({
-            id: item.id,
-            nombre: item.nombre,
-            descripcion: item.descripcion,
-            imagen: item.imagen
-        }));
-
-        //console.log("exito al recuperar productos: ", data);
-        res.status(200).json(data);
+        if(productos.length === 0){
+            res.status(404).json({message: "No se encontraron productos de esta marca!"});
+        }else{
+            const data = productos.map((item)=>({
+                id: item.id,
+                nombre: item.nombre,
+                descripcion: item.descripcion,
+                imagen: item.imagen
+            }));
+            res.status(200).json(data);
+        }
     } catch (error) {
-        console.log("error al traer los productos: ", error);
-        res.status(500).json({error: 'Fallo al traer productos!'});
+        console.log("Error al recuperar productos por marca id: ", error);
+        res.status(500).json({error: 'Error al recuperar productos por marca id!'});
     }
 });
 
 router.get('/producto/:id', async(req, res)=>{
     
     try {
-        const response = await Productos.findByPk(req.params.id);
-        if(response === null){
-            //console.log("Not Found!****************");
+        const producto = await Productos.findByPk(req.params.id);
+        if(producto === null){
+            res.status(404).json({message: "No se encontro el producto!"});
         }else{
-            //console.log("soy response product_id: ", response);
             res.status(200).json(response);
         }
     } catch (error) {
-        console.log("error al recuperar producto por id: ", error);
+        console.log("Error al recuperar producto por id: ", error);
+        res.status(500).json({message: "Error al recuperar producto por id!"});
     }
 });
 
 router.get('/productoByCategory/:id', async (req, res)=>{
-    console.log("HHHHHHHHHHHHHHHHHHHHHHHHH")
+    
     const id = req.params.id;
     try {
         const productos = await Productos.findAll({
@@ -151,25 +150,27 @@ router.get('/productoByCategory/:id', async (req, res)=>{
             order: ['nombre'],
             limit: 15,
         });
-        const data = productos.map((item)=>({
-            id: item.id,
-            nombre: item.nombre,
-            descripcion: item.descripcion,
-            imagen: item.imagen
-        }));
-        res.status(200).json(data);
+        if(productos.length === 0){
+            res.status(404).json({message: "No se encontraron productos de esta categoria"});
+        }else{
+            const data = productos.map((item)=>({
+                id: item.id,
+                nombre: item.nombre,
+                descripcion: item.descripcion,
+                imagen: item.imagen
+            }));
+            res.status(200).json(data);
+        }
     } catch (error) {
-        res.status(500).json({message: 'error al recuperar productos por categoria.'});
+        console.log("Error al recuperar productos por categoria: ", error);
+        res.status(500).json({message: 'Error al recuperar productos por categoria.'});
     }
 });
 
-router.post('/newproduct', upload.single("imagen"), async(req, res)=>{
-    //console.log("reqBodyyyyyyyyyy", req.body);
+router.post('/newProduct', upload.single("imagen"), async(req, res)=>{
 
     try {
         const{ path } = req.file;
-
-
         const {nombre, descripcion, categoriaId, marcaId} = req.body;
 
         const newProducto = {
@@ -179,22 +180,17 @@ router.post('/newproduct', upload.single("imagen"), async(req, res)=>{
             CategoriaId: categoriaId,
             MarcaId: marcaId
         }
-        console.log("Producto :", newProducto);
-
+        //console.log("Producto :", newProducto);
         const response = await Productos.create(newProducto);
-
-        console.log("Producto creado con exito!", response);
         
-        res.status(200).json({message: 'todo salio bien!'});
+        res.status(200).json({message: 'El producto se creo con extito!'});
 
     } catch (error) {
-
-        console.log("error al guardar nuevo producto en la db: ", error);
-        res.status(500).json({message: 'no se pudo cargar los datos!'});
+        console.log("Error al crear nuevo producto: ", error);
+        res.status(500).json({message: 'Error al crear nuevo producto!'});
     }
-
 });
-
+/*
 router.put('/edit-producto/:id', upload.none(), async(req, res)=>{
     try {
         
@@ -211,10 +207,8 @@ router.put('/edit-producto/:id', upload.none(), async(req, res)=>{
     }
 
 });
-
-router.delete('/delete-producto/:id', async(req, res)=>{
-    
-    console.log("estoy en delete product", req.params.id);
+*/
+router.delete('/deleteProduct/:id', async(req, res)=>{
 
     try {
         const response = await Productos.destroy({
@@ -224,8 +218,8 @@ router.delete('/delete-producto/:id', async(req, res)=>{
         });
         res.status(200).json({message: 'Producto borrado con exito!'});
     } catch (error) {
-        console.log("no se pudo borrar el producto: ", error);
-        res.status(500).json({message: 'error al borrar producto!'});
+        console.log("Error al borrar producto: ", error);
+        res.status(500).json({message: 'Error al borrar producto!'});
     }
 
 });
