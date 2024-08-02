374c64babf82a3ddec57e9b67948315b85de4f5e
Sebastian Rossi <ssebass.ross@gmail.com>
2024-08-02 01:56:58 -0300

    se modifico insert_seeds.js y marcas.json

--- src/seeds/insert_seeds.js
+++ src/seeds/insert_seeds.js
@@ -26,5 +26,67 @@ async function insert_seeds(ruta, tabla){
     }
 }
 
-insert_seeds('categorias.json', Categorias);
-insert_seeds('marcas.json', Marcas);
+//insert_seeds('categorias.json', Categorias);
+
+
+// Función para leer y parsear el archivo .json
+function readJsonFile(ruta) {
+    try {
+        const filePath = path.join(__dirname, `${ruta}`);
+        const data = fs.readFileSync(filePath, 'utf8');      
+        return JSON.parse(data);
+    } catch (error) {
+        console.error(`Error leyendo el archivo: `, error);
+        return null;
+    }
+}
+function writeJsonFile(ruta, data) {
+    try {
+        const filePath = path.join(__dirname, `${ruta}`);
+        const jsonData = JSON.stringify(data, null, 2);
+        fs.writeFileSync(filePath, jsonData, 'utf8');
+    } catch (error) {
+        console.error(`Error escribiendo en el archivo ${ruta}:`, error);
+    }
+}
+
+async function addCategoryToMarcasJson(list_marcas, marcas_to_db) {
+    // Leer el archivo JSON
+    try {
+        const data = await readJsonFile(list_marcas);
+        if (!data) {
+            console.error(`No se pudo leer el archivo ${list_marcas}.`);
+            return null;
+        }
+        console.log("dataaaaa:::::: ", data);
+
+        const category = await Categorias.findAll();
+
+        const newJson = [];
+
+        // Iterar sobre cada elemento del JSON
+        data.forEach(item => {
+            //comparar y modificar un valor según alguna condición
+            category.forEach(valor=>{
+                if(item.CategoriaId === valor.dataValues.nombre){
+                    item.CategoriaId = valor.dataValues.id
+                }
+            })
+            newJson.push(item);
+        });
+        // Escribir el JSON modificado de vuelta al archivo
+        writeJsonFile(marcas_to_db, newJson);
+        console.log(`JSON modificado y guardado en ${marcas_to_db}.`);
+
+    } catch (error) {
+        console.log("Error al editar CategoriaId en marcas_to_db.json: ", error);
+        return null;
+    }
+}
+
+if(addCategoryToMarcasJson('list_marcas.json', 'maracs_to_db.json') != null){
+    insert_seeds('marcas_to_db.json', Marcas);
+}
+
+
+
